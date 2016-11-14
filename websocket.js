b80c666b1443054dc761060ea8a2aa0cfa17405c
Josh Rowley <joshua.f.rowley@gmail.com>
2016-11-14 10:58:50 -0500

    fix issue where multiple browser windows spawn

--- websocket.js
+++ websocket.js
@@ -1,62 +1,79 @@
 const bus = require('page-bus')()
 const websockets = {}
+const managers = []
 
-window.onbeforeunload = () => {
-  localStorage.removeItem('socket:drawer:running')
-}
+console.log(`current process is ${process.pid}`)
 
-getSocket = (key, url) => {
-  var ws = new WebSocket(url)
-  websockets[key] = ws
-
-  ws.onopen = () => {
-    console.log(`websocket open for ${key}: ${url}`)
-    bus.emit(`${key}:open`)
-  }
-
-  ws.onmessage = (msg) => {
-    console.log(`received message for ${key}: ${url}`, msg.data)
-    bus.emit(`${key}:message`, msg.data)
-  }
-
-  ws.onerror = (err) => {
-    delete websockets[key]
-    console.log(`error  for ${key}: ${url}`, err)
-    bus.emit(`${key}:error`, err)
-  }
-
-  ws.onclose = () => {
-    delete websockets[key]
-    console.log(`websocket close for ${key}: ${url}`)
-    bus.emit(`${key}:close`)
-  }
-
-  return ws
-}
-
-bus.on('create', ({key, url}) => {
-  console.log(`received request for ${key}: ${url}`)
-  if (websockets[key]) {
-    console.log(`found websocket from cache for ${key}: ${url}`)
-  } else {
-    console.log(`creating new websocket for ${key}: ${url}`)
-
-    var ws = getSocket(key, url)
-
-    bus.on(`${key}:send`, (msg) => {
-      console.log(`sending message for ${key}: ${url}`, msg)
-      ws.send(msg)
-    })
-
-    bus.on(`${key}:close:request`, () => {
-      console.log(`closing websocket for ${key}: ${url}`)
-      ws.close()
-    })
-
-    bus.on(`${key}:reset:request`, () => {
-      console.log(`resetting websocket for ${key}: ${url}`)
-      ws.close()
-      ws = getSocket(key, url)
-    })
-  }
+bus.on('websocket:manager:start', ({pid, time}) => {
+  console.log(`process ${pid} started at ${time}`)
+  managers.push({pid: pid, time: time})
 })
+
+bus.emit('websocket:manager:start', {pid: process.pid, time: Date.now()})
+
+setTimeout(() => {
+  if (managers.length > 1) {
+    window.close()
+    return process.exit(0)
+  }
+
+  window.onbeforeunload = () => {
+    localStorage.removeItem('socket:drawer:running')
+  }
+
+  getSocket = (key, url) => {
+    var ws = new WebSocket(url)
+    websockets[key] = ws
+
+    ws.onopen = () => {
+      console.log(`websocket open for ${key}: ${url}`)
+      bus.emit(`${key}:open`)
+    }
+
+    ws.onmessage = (msg) => {
+      console.log(`received message for ${key}: ${url}`, msg.data)
+      bus.emit(`${key}:message`, msg.data)
+    }
+
+    ws.onerror = (err) => {
+      delete websockets[key]
+      console.log(`error  for ${key}: ${url}`, err)
+      bus.emit(`${key}:error`, err)
+    }
+
+    ws.onclose = () => {
+      delete websockets[key]
+      console.log(`websocket close for ${key}: ${url}`)
+      bus.emit(`${key}:close`)
+    }
+
+    return ws
+  }
+
+  bus.on('create', ({key, url}) => {
+    console.log(`received request for ${key}: ${url}`)
+    if (websockets[key]) {
+      console.log(`found websocket from cache for ${key}: ${url}`)
+    } else {
+      console.log(`creating new websocket for ${key}: ${url}`)
+
+      var ws = getSocket(key, url)
+
+      bus.on(`${key}:send`, (msg) => {
+        console.log(`sending message for ${key}: ${url}`, msg)
+        ws.send(msg)
+      })
+
+      bus.on(`${key}:close:request`, () => {
+        console.log(`closing websocket for ${key}: ${url}`)
+        ws.close()
+      })
+
+      bus.on(`${key}:reset:request`, () => {
+        console.log(`resetting websocket for ${key}: ${url}`)
+        ws.close()
+        ws = getSocket(key, url)
+      })
+    }
+  })
+}, 1000)
